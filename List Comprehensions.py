daa8e0b1a86ee4b630b66b6fe3a1981c28da64ea
YeahPotato <277637411@qq.com>
2018-08-08 17:47:48 +0800

    Update

--- List Comprehensions.py
+++ List Comprehensions.py
@@ -1,7 +1,6 @@
 # 列表生成式
 import os
 
-
 l = [d for d in os.listdir('.')]
 a = [m+n for m in 'ABC' for n in 'abc']
 print(a)
@@ -10,3 +9,4 @@ print(a)
 L1 = ['Hello', 'World', 18, 'Apple', None]
 l2 = [n.lower() for n in L1 if isinstance(n,str)]
 print(l2)
+
